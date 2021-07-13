b19b854b0adc399d1fb40d84a79d6a21787576bd
tamumisa <69777343+tamumisa@users.noreply.github.com>
2021-07-13 17:00:43 +0900

    DBのコードに入りました
    次回：DB初期化から
    コードの準備は整ったので、早速DB作成　コマンド操作

---
+++ practice/models/database.py
@@ -0,0 +1,23 @@
+#DBとの直接的な接続の方法を格納します
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session,sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import onegaicontents
+
+#DBへの接続情報を定義している
+#①datebase.pyと同じパスに onegai.dbというファイルを絶対パスで定義
+datebase_file = os.path.join(os.path.dirname(__file__)),'onegai.db')
+#②SQliteを利用して1.で定義した絶対パスにDBを構築
+engine = create_engine('sqlite:///' + database_file,convert_unicode=True)
+#③DB接続用インスタンスを生成
+db_session = scoped_session(sessionmaker(autocommit=False,autoflush=False,bind=engine))
+#Baseオブジェクトを生成して
+Base = declarative_baes()
+#そこにDBの情報を流し込む
+Baes.query = db_session.query_property()
+
+#DB初期化のための関数を定義
+def init_db():
+    import models.models
+    Base.metadate.create_all(bind=engine)
---
+++ practice/models/models.py
@@ -0,0 +1,28 @@
+#テーブルのカラム情報を定義するためのクラスを格納します。
+#テーブル操作を行う際のレコード生成もこのクラスを通して行います。
+#カラム構成
+#ID(int:キー情報)
+#title(String)(128)お願いのタイトル)
+#body(text:お願いの内容)
+#date(datetime:お願いの投稿日時)
+
+from sqlalchemy import Clumn,String,Text,DateTime
+from models.database import Base
+from datetime import datetime
+
+#カラム情報の定義を行う
+#テーブル名とカラム別にカラム名と方を使用しています
+#ここで引数に渡しているBaseは次のdatebase.pyで作るインスタンスなので後で説明らしい（笑）
+class Onegaicontent(Base):
+    __tablename__ = "onegaicontents"
+    id = Column(String(128),unique=True)
+    body = Column(Text)
+    date = Column(Datetime,default=datetime.now()))
+
+    def __init__(self,title=None,body=None,date=None):
+        self.title = title
+        self.body = body
+        self.date = date
+
+    def __repr__(self)
+    return '<Title %r' %(self.title)
